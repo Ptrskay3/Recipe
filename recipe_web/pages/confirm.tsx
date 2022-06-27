e24accaad17ac6efa050946c311dc59043ad9e77
peter leeh <leeh123peter@gmail.com>
2022-06-27 09:11:46 +0200

    some adjustment

--- recipe_web/pages/confirm.tsx
+++ recipe_web/pages/confirm.tsx
@@ -1,32 +1,58 @@
-import { Box, Center, Heading, Link, Text } from '@chakra-ui/react';
-import { CheckCircleIcon } from '@chakra-ui/icons';
+import { Box, Center, CircularProgress, Flex, Heading, Link, Text } from '@chakra-ui/react';
+import { CheckCircleIcon, CloseIcon } from '@chakra-ui/icons';
 import NextLink from 'next/link';
 import { Layout } from '../components/layout';
-import dynamic from 'next/dynamic';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
-import { fetcher } from '../utils/fetcher';
+import { fetcherWithoutJson } from '../utils/fetcher';
 
-function Confirm() {
+export default function Confirm() {
   const router = useRouter();
   const { token } = router.query;
 
   const { data, error } = useSWR(
     !!token ? `http://localhost:3000/confirm?token=${token}` : null,
-    fetcher
+    fetcherWithoutJson,
+    {
+      shouldRetryOnError: false,
+    }
   );
-  if (!token)
+  if (!error && !data && !!token) {
     return (
       <Layout>
-        <Center mt={12}>{'nice try'}</Center>
+        <Center mt="14">
+          <CircularProgress isIndeterminate color="orange.400" />
+        </Center>
       </Layout>
     );
-  if (!data || error)
-    return (
-      <Layout>
-        <Center mt={12}>{'Token is invalid, likely expired'}</Center>
-      </Layout>
-    );
+  }
+
+  // TODO: The logic is somewhat wrong for this.
+  // if (data && !!token && (error || !data.ok))
+  //   return (
+  //     <Layout>
+  //       <Box textAlign="center" py={10} px={6}>
+  //         <Box display="inline-block">
+  //           <Flex
+  //             flexDirection="column"
+  //             justifyContent="center"
+  //             alignItems="center"
+  //             bg={'red.500'}
+  //             rounded={'50px'}
+  //             w={'55px'}
+  //             h={'55px'}
+  //             textAlign="center"
+  //           >
+  //             <CloseIcon boxSize={'20px'} color={'white'} />
+  //           </Flex>
+  //         </Box>
+  //         <Heading as="h2" size="xl" mt={6} mb={2}>
+  //           Something went wrong.
+  //         </Heading>
+  //         <Text color={'gray.500'}>The provided token is invalid, or expired.</Text>
+  //       </Box>
+  //     </Layout>
+  //   );
   return (
     <Layout>
       <Box textAlign="center" py={10} px={6}>
@@ -44,5 +70,3 @@ function Confirm() {
     </Layout>
   );
 }
-
-export default dynamic(() => Promise.resolve(Confirm), { ssr: false });
--- recipe_web/utils/fetcher.ts
+++ recipe_web/utils/fetcher.ts
@@ -1,3 +1,5 @@
 // @ts-nocheck
 export const fetcher = (...args) =>
   fetch(...args, { credentials: 'include' }).then((res) => res.json());
+
+export const fetcherWithoutJson = (...args) => fetch(...args, { credentials: 'include' });
