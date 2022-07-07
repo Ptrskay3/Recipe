2637df63cc5459a5e8f72634c6cc4bca30760981
peter leeh <leeh123peter@gmail.com>
2022-07-07 10:40:38 +0200

    p..

--- axum1/src/routes/recipe/mod.rs
+++ axum1/src/routes/recipe/mod.rs
@@ -1,6 +1,7 @@
 use anyhow::Context;
 use axum::{extract::Path, routing::get, Json, Router};
 use axum_extra::extract::Form;
+use sqlx::Acquire;
 
 use crate::{
     error::ApiError,
@@ -8,30 +9,77 @@ use crate::{
 };
 
 pub fn recipe_router() -> Router {
-    Router::new().route("/:name", get(get_recipe).post(insert_recipe))
+    Router::new().route(
+        "/:name",
+        get(get_recipe_with_ingredients).post(insert_recipe),
+    )
+}
+
+#[derive(Debug, Clone, serde::Deserialize, serde::Serialize, sqlx::FromRow)]
+struct RecipeWithIngredients {
+    name: String,
+    description: String,
+    ingredients: Vec<DetailedIngredient>,
+}
+
+#[derive(Debug, Clone, serde::Deserialize, serde::Serialize, sqlx::FromRow)]
+
+struct DetailedIngredient {
+    name: String,
+    quantity: String,
+    quantity_unit: String,
+}
+
+#[tracing::instrument(skip(conn))]
+async fn get_recipe_with_ingredients(
+    DatabaseConnection(mut conn): DatabaseConnection,
+    Path(name): Path<String>,
+) -> Result<Json<RecipeWithIngredients>, ApiError> {
+    let mut tx = conn.begin().await?;
+
+    // A little bit clunky, but better be safe than overly smart.
+    let recipe = sqlx::query_as!(
+        Recipe,
+        r#"
+        SELECT name, description
+        FROM recipes
+        WHERE name = $1
+        "#,
+        name
+    )
+    .fetch_optional(&mut tx)
+    .await
+    .context("Failed to query recipe")?
+    .ok_or(ApiError::NotFound)?;
+
+    let ingredients: Vec<DetailedIngredient> = sqlx::query_as!(
+        DetailedIngredient,
+        r#"
+        SELECT i.name, ir.quantity, ir.quantity_unit FROM recipes r
+        INNER JOIN ingredients_to_recipes ir
+        ON r.id = ir.recipe_id
+        INNER JOIN ingredients i
+        ON i.id = ir.ingredient_id
+        WHERE r.name = $1;
+        "#,
+        name
+    )
+    .fetch_all(&mut tx)
+    .await
+    .context("Failed to query recipe ingredients")?;
+    tx.commit().await?;
+
+    Ok(Json(RecipeWithIngredients {
+        ingredients,
+        name: recipe.name,
+        description: recipe.description,
+    }))
 }
 
 #[derive(Debug, Clone, serde::Deserialize, serde::Serialize, sqlx::FromRow)]
 struct Recipe {
     name: String,
     description: String,
-}
-
-async fn get_recipe(
-    DatabaseConnection(mut conn): DatabaseConnection,
-    Path(name): Path<String>,
-) -> Result<Json<Recipe>, ApiError> {
-    let recipe = sqlx::query_as!(
-        Recipe,
-        r#"SELECT name, description FROM recipes WHERE name = $1"#,
-        name
-    )
-    .fetch_optional(&mut conn)
-    .await
-    .context("Failed to query for recipe")?
-    .ok_or(ApiError::NotFound)?;
-
-    Ok(Json(recipe))
 }
 
 async fn insert_recipe(
