592896807501c79f00507c8ad7dca52b3bcd0f13
Leéh Péter <leeh123peter@gmail.com>
2022-07-10 12:57:28 +0200

    make lighthouse happy

--- recipe_web/pages/_app.tsx
+++ recipe_web/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../common/theme';
 import { useEffect, useState } from 'react';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [mounted, setMounted] = useState(false);
@@ -12,9 +13,15 @@ function MyApp({ Component, pageProps }: AppProps) {
   }, []);
 
   const body = (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>Recipes</title>
+        <meta name="description" content="A recipe app made with Rust and Next.js"></meta>
+      </Head>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 
   // prevents ssr flash for mismatched dark mode
