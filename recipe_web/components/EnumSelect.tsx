d83d5e0d3b28e455340e9dbd74d4522c9cec012e
peter leeh <leeh123peter@gmail.com>
2022-07-19 10:05:55 +0200

    add widgets

---
+++ recipe_web/components/EnumSelect.tsx
@@ -0,0 +1,70 @@
+import { Box, HStack, useRadio, useRadioGroup } from '@chakra-ui/react';
+
+function RadioCard(props: any) {
+  const { getInputProps, getCheckboxProps } = useRadio(props);
+
+  const input = getInputProps();
+  const checkbox = getCheckboxProps();
+
+  return (
+    <Box as="label">
+      <input {...input} />
+      <Box
+        {...checkbox}
+        cursor="pointer"
+        borderWidth="1px"
+        borderRadius="md"
+        boxShadow="md"
+        _checked={{
+          bg: 'orange.400',
+          color: 'white',
+          borderColor: 'orange.400',
+        }}
+        _focus={{
+          boxShadow: 'outline',
+        }}
+        px={5}
+        py={3}
+      >
+        {props.children}
+      </Box>
+    </Box>
+  );
+}
+
+export function EnumSelector<T>({
+  options,
+  name,
+  defaultValue,
+  onChange,
+}: {
+  options: readonly T[];
+  name: string;
+  defaultValue: T;
+  onChange?: (...args: any) => any;
+}) {
+  const { getRootProps, getRadioProps } = useRadioGroup({
+    name: name,
+    defaultValue: defaultValue as any,
+    onChange: onChange,
+  });
+
+  const group = getRootProps();
+
+  return (
+    <HStack {...group}>
+      {options.map((value) => {
+        const radio = getRadioProps({ value } as any);
+        return (
+          <RadioCard key={value} {...radio}>
+            {toPretty(value as unknown as string)}
+          </RadioCard>
+        );
+      })}
+    </HStack>
+  );
+}
+
+function toPretty(s: string): string {
+  return s.replaceAll('_', ' ');
+}
