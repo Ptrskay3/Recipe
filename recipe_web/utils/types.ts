0ee98d8cd08cd619a47e5fdca14d63045aae3b1c
peter leeh <leeh123peter@gmail.com>
2022-07-19 09:09:27 +0200

    start adding elements to new recipe

---
+++ recipe_web/utils/types.ts
@@ -0,0 +1,10 @@
+export type MealType = 'breakfast' | 'lunch' | 'dinner' | 'other';
+
+export type DifficultyLevel =
+  | 'easy'
+  | 'moderate'
+  | 'medium'
+  | 'challenging'
+  | 'hard'
+  | 'extreme'
+  | 'do_not_attempt';
