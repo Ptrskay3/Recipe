6c5720c775c98df50d6c6d3c9f0fcb07fb7bb8c4
peter leeh <leeh123peter@gmail.com>
2022-07-20 08:49:03 +0200

    add useAddIngredient store

---
+++ recipe_web/stores/useAddRecipe.ts
@@ -0,0 +1,33 @@
+import create from 'zustand';
+import { combine } from 'zustand/middleware';
+import { DifficultyLevel, MealType } from '../utils/types';
+
+export const useAddIngredient = create(
+  combine(
+    {
+      name: '',
+      description: '',
+      prep_time: 0,
+      cook_time: 0,
+      difficulty: 'easy',
+      steps: [] as string[],
+      cuisine: 'hungarian',
+      meal_type: 'breakfast' as MealType,
+      ingredients: [] as any[], // TODO: type
+    },
+    (set) => ({
+      setName: (value: string) => set(() => ({ name: value })),
+      setDescription: (value: string) => set(() => ({ description: value })),
+      setPrepTime: (value: number) => set(() => ({ prep_time: value })),
+      setCookTime: (value: number) => set(() => ({ cook_time: value })),
+      setDifficulty: (value: DifficultyLevel) => set(() => ({ difficulty: value })),
+      setSteps: (value: string[]) => set(() => ({ steps: value })),
+      pushStep: (value: string) => set(({ steps }) => ({ steps: [...steps, value] })),
+      setCuisine: (value: string) => set(() => ({ cuisine: value })),
+      setMealType: (value: MealType) => set(() => ({ meal_type: value })),
+      setIngredients: (value: any[]) => set(() => ({ ingredients: value })),
+      pushIngredient: (value: any) =>
+        set(({ ingredients }) => ({ ingredients: [...ingredients, value] })),
+    })
+  )
+);
