c0ad0e3fc46c34fdd7d4dfeb2488c3374c94e568
peter leeh <leeh123peter@gmail.com>
2022-07-20 12:25:08 +0200

    add a little responsivity

--- recipe_web/components/slider.tsx
+++ recipe_web/components/slider.tsx
@@ -5,12 +5,15 @@ import {
   SliderFilledTrack,
   Tooltip,
   SliderThumb,
+  useMediaQuery,
 } from '@chakra-ui/react';
 import { useState } from 'react';
 
 export function DurationSlider({ onChangeEnd }: { onChangeEnd?: (...args: any) => any }) {
   const [sliderValue, setSliderValue] = useState(5);
   const [showTooltip, setShowTooltip] = useState(false);
+  const [isLargerThan900] = useMediaQuery('(min-width: 900px)');
+  const suffix = isLargerThan900 ? 'mins' : 'm';
   return (
     <Slider
       id="slider"
@@ -23,23 +26,25 @@ export function DurationSlider({ onChangeEnd }: { onChangeEnd?: (...args: any) =
       onMouseLeave={() => setShowTooltip(false)}
       onChangeEnd={onChangeEnd}
     >
-      <SliderMark value={10} mt="1" ml="-2.5" fontSize="sm">
-        10 mins
-      </SliderMark>
+      {isLargerThan900 ? (
+        <SliderMark value={10} mt="1" ml="-2.5" fontSize="sm">
+          10 {suffix}
+        </SliderMark>
+      ) : null}
       <SliderMark value={30} mt="1" ml="-2.5" fontSize="sm">
-        30 mins
+        30 {suffix}
       </SliderMark>
       <SliderMark value={60} mt="1" ml="-2.5" fontSize="sm">
-        60 mins
+        60 {suffix}
       </SliderMark>
       <SliderMark value={90} mt="1" ml="-2.5" fontSize="sm">
-        90 mins
+        90 {suffix}
       </SliderMark>
       <SliderMark value={120} mt="1" ml="-2.5" fontSize="sm">
-        120 mins
+        120 {suffix}
       </SliderMark>
       <SliderMark value={180} mt="1" ml="-2.5" fontSize="sm">
-        180 mins
+        180 {suffix}
       </SliderMark>
       <SliderTrack>
         <SliderFilledTrack />
