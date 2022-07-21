14402a3b4c6958392411f326a6402b5daee57662
peter leeh <leeh123peter@gmail.com>
2022-07-21 11:11:49 +0200

    search

---
+++ recipe_web/components/search/SearchBox.tsx
@@ -0,0 +1,17 @@
+import { Input } from '@chakra-ui/react';
+import { useSearchBox } from 'react-instantsearch-hooks-web';
+
+export function CustomSearchBox(props: any) {
+  const { query, refine } = useSearchBox(props);
+  return (
+    <Input
+      maxW={'240px'}
+      type="text"
+      placeholder="Search"
+      value={query}
+      onChange={(e) => {
+        refine(e.target.value);
+      }}
+    />
+  );
+}
