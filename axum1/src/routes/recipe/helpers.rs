750aed225474d7b0b27d993cd82d9dd230b4ee0d
peter leeh <leeh123peter@gmail.com>
2022-07-22 10:52:36 +0200

    .

--- axum1/src/routes/recipe/helpers.rs
+++ axum1/src/routes/recipe/helpers.rs
@@ -35,3 +35,44 @@ impl PgHasArrayType for TypeByTime {
         PgTypeInfo::with_name("_type_by_time")
     }
 }
+
+#[derive(Clone, Debug, serde::Serialize, serde::Deserialize, Default)]
+#[allow(non_camel_case_types)]
+pub enum QuantityUnit {
+    g,
+    mg,
+    kg,
+    #[default]
+    Empty,
+}
+
+impl<'a> TryFrom<&'a str> for QuantityUnit {
+    type Error = validator::ValidationError;
+
+    fn try_from(value: &'a str) -> Result<Self, Self::Error> {
+        match value {
+            "g" | "G" => Ok(Self::g),
+            "kg" | "Kg" | "KG" | "kG" => Ok(Self::kg),
+            "mg" | "Mg" | "MG" | "mG" => Ok(Self::mg),
+            "" => Ok(Self::Empty),
+            other => {
+                let mut err = validator::ValidationError::new("quantity_unit");
+                err.message = Some(std::borrow::Cow::from(
+                    "Cannot parse unit, only `g`, `mg` and `kg` are allowed.",
+                ));
+                err.add_param(std::borrow::Cow::from("quantity_unit"), &other);
+                Err(err)
+            }
+        }
+    }
+}
+
+impl QuantityUnit {
+    pub fn get_multiplier_for_g(&self) -> f32 {
+        match self {
+            QuantityUnit::g | QuantityUnit::Empty => 1.0, // TODO: How do we handle empty?
+            QuantityUnit::mg => 0.01,
+            QuantityUnit::kg => 100.0,
+        }
+    }
+}
