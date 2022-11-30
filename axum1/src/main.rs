aaea02a3982e9ab579798ab1e5e090eb8260e2a1
peter leeh <leeh123peter@gmail.com>
2022-11-30 08:45:52 +0100

    style: tracing_subsciber from default env

--- axum1/src/main.rs
+++ axum1/src/main.rs
@@ -27,12 +27,12 @@ async fn main() -> anyhow::Result<()> {
 
     let application_task = tokio::spawn(application());
     let worker_task = tokio::spawn(run_worker_until_stopped(configuration.clone()));
-    let meili_indexing_task = tokio::spawn(run_meili_indexer_until_stopped(configuration));
+    // let meili_indexing_task = tokio::spawn(run_meili_indexer_until_stopped(configuration));
 
     tokio::select! {
         f = application_task => report_exit("Server", f),
         f = worker_task =>  report_exit("Background worker", f),
-        f = meili_indexing_task =>  report_exit("Meili indexer", f),
+        // f = meili_indexing_task =>  report_exit("Meili indexer", f),
     };
 
     Ok(())
